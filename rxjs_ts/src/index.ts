1797a24e3ea8ae33124a7c90f26ec439e7d20e7b
cyberspacedk <frontend.dk@gmail.com>
2020-09-13 12:03:41 +0300

    drag-n-drop

--- rxjs_ts/src/index.ts
+++ rxjs_ts/src/index.ts
@@ -1,6 +1,13 @@
-import {Observable, interval, fromEvent, Subscriber, of, pipe} from 'rxjs';
+import {Observable, interval, fromEvent, Subscriber, of, pipe, zip} from 'rxjs';
 import {map, take, filter, pluck, mergeAll, mergeMap, switchAll, debounceTime, concatAll, exhaust} from 'rxjs/operators';  
 import { ajax } from 'rxjs/ajax';
 
 // import './live-search';
-import './drag_n_drop'
+// import './drag_n_drop'
+
+const sequence1$ = interval(500);
+const sequence2$ = of(1,2,3,4,5,6,7);
+
+const combinedSequences$ = zip(sequence1$, sequence2$);
+
+combinedSequences$.subscribe(console.log)
